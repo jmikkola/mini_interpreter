e293bacd6fc3e12d5210b583cd0ab48609e79097
Jeremy Mikkola <jeremy@jeremymikkola.com>
2016-11-03 23:41:11 -0700

    Start parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{stdin, BufReader, BufRead};
 use std::collections::HashMap;
 
-#[derive(Debug, Clone)]
+#[derive(PartialEq, Debug, Clone)]
 enum Value {
     IntVal(i64),
     FloatVal(f64),
@@ -10,7 +10,7 @@ enum Value {
     BoolVal(bool),
 }
 
-#[derive(Debug, Clone)]
+#[derive(PartialEq, Debug, Clone)]
 enum Command {
     Push(Value),
     Pop,
@@ -30,22 +30,58 @@ enum Command {
 }
 
 fn read_lines() -> Vec<String> {
-    BufReader::new(stdin()).lines()
+    BufReader::new(stdin())
+        .lines()
         .map(|l| l.unwrap().trim().to_owned())
         .collect()
 }
 
+fn parse_err<a>(msg: &str) -> Result<a, String> {
+    Err(msg.to_owned())
+}
+
+fn parse_value(parts: &Vec<&str>) -> Result<Value, String> {
+    parse_err("not implemented")
+}
+
 fn parse_command(line: String) -> Result<Command, String> {
-    Ok(Command::Dup)
+    let parts: Vec<&str> = line.split_whitespace().collect();
+    if parts.len() < 1 {
+        return parse_err("empty command?");
+    }
+    match parts[0] {
+        "push" => {
+            if parts.len() > 2 {
+                Ok(Command::Push(try!(parse_value(&parts))))
+            } else {
+                parse_err("invalid push statement")
+            }
+        }
+        "pop" => Ok(Command::Pop),
+        "dup" => Ok(Command::Dup),
+        "plus" => Ok(Command::Plus),
+        "minus" => Ok(Command::Minus),
+        "times" => Ok(Command::Times),
+        "divide" => Ok(Command::Divide),
+        "mod" => Ok(Command::Mod),
+        "print" => Ok(Command::Print),
+        "mktype" => Ok(Command::MkType),
+        "new" => Ok(Command::New),
+        "getref" => Ok(Command::GetRef),
+        "setref" => Ok(Command::SetRef),
+        "sysgc" => Ok(Command::SysGC),
+        "sysmemstats" => Ok(Command::SysMemstats),
+        _ => Err("invalid command: ".to_owned() + line.as_str()),
+    }
 }
 
 fn build(lines: Vec<String>) -> Result<(Vec<Command>, HashMap<String, usize>), String> {
     let mut commands = vec![];
     let mut names = HashMap::new();
 
-    for mut line in lines {
+    for line in lines {
         if line.len() == 0 || line.starts_with("#") {
-            continue
+            continue;
         } else if line.starts_with("!") {
             names.insert(line.trim_left_matches("!").to_owned(), commands.len());
         } else {
