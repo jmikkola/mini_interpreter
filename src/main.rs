a18469cfe92d70f2fa4f4f9206913b2d4bc7f23b
Jeremy Mikkola <jeremy@jeremymikkola.com>
2016-11-05 15:08:13 -0700

    Start adding functions

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,15 @@ impl Value {
 }
 
 #[derive(PartialEq, Debug, Clone)]
+struct Function {
+    name: String,
+    commands: Vec<Command>,
+    names: HashMap<String, usize>,
+    n_args: i32,
+    n_locals: i32,
+}
+
+#[derive(PartialEq, Debug, Clone)]
 enum Command {
     Push(Value),
     Pop,
@@ -116,21 +125,45 @@ fn parse_command(line: String) -> Result<Command, String> {
     }
 }
 
-fn build(lines: Vec<String>) -> Result<(Vec<Command>, HashMap<String, usize>), String> {
-    let mut commands = vec![];
-    let mut names = HashMap::new();
+fn parse_fn(line: String) -> Result<Function, String> {
+    let parts: Vec<&str> = line.split_whitespace().collect();
+    if parts.len() != 3 {
+        return parse_err("invalid function def");
+    }
+
+    Ok(Function {
+        name: parts[1].to_owned(),
+        commands: Vec::new(),
+        names: HashMap::new(),
+        n_locals: 0, // TODO?
+        n_args: try!(parts[2].parse().map_err(|_| "can't parse int")),
+    })
+}
+
+fn build(lines: Vec<String>) -> Result<HashMap<String, Function>, String> {
+    let mut functions = vec![];
 
     for line in lines {
         if line.len() == 0 || line.starts_with("#") {
             continue;
+        } else if line.starts_with(".F") {
+            functions.push(try!(parse_fn(line)));
         } else if line.starts_with("!") {
-            names.insert(line.trim_left_matches("!").to_owned(), commands.len());
+            if let Some(mut f) = functions.last_mut() {
+                let name = line.trim_left_matches("!").to_owned();
+                let location = f.commands.len();
+                f.names.insert(name, location);
+            }
         } else {
-            commands.push(try!(parse_command(line)))
+            if let Some(mut f) = functions.last_mut() {
+                let command = try!(parse_command(line));
+                // TODO: check for local var frame size?
+                f.commands.push(command);
+            }
         }
     }
 
-    Ok((commands, names))
+    Ok(functions.into_iter().map(|f| (f.name.clone(), f)).collect())
 }
 
 enum MathOp {
@@ -180,84 +213,105 @@ fn default_value(t: &String) -> Value {
     }
 }
 
-fn interpret(commands: Vec<Command>, names: HashMap<String, usize>) {
-    let mut stack: Vec<Value> = vec![];
-    let mut heap: HashMap<i64, Vec<Value>> = HashMap::new();
+#[derive(Debug)]
+struct Frame {
+    stack: Vec<Value>,
+    locals: Vec<Value>,
+    prev_pc: i64,
+    prev_fn_name: String,
+}
+
+fn new_frame(prev_pc: i64, prev_fn_name: String) -> Frame {
+    Frame {
+        stack: vec![],
+        locals: vec![],
+        prev_pc: prev_pc,
+        prev_fn_name: prev_fn_name,
+    }
+}
+
+fn interpret(functions: HashMap<String, Function>) {
     let mut types = vec![];
+
+    //let mut stack: Vec<Frame> = vec![];
+    let mut frame = new_frame(0, "_init_".to_owned());
+
+    let mut heap: HashMap<i64, Vec<Value>> = HashMap::new();
     let mut next_obj_id = 1;
 
+    let f = functions.get(&"main".to_owned()).unwrap();
     let mut pc = 0;
-    while pc < commands.len() {
-        let cmd = &commands[pc];
+    while pc < f.commands.len() {
+        let cmd = &f.commands[pc];
         pc += 1;
 
         match *cmd {
             Command::Push(ref v) => {
-                stack.push(v.clone());
+                frame.stack.push(v.clone());
             }
             Command::Pop => {
-                stack.pop();
+                frame.stack.pop();
             }
             Command::Dup => {
-                let top_val = stack.last().unwrap().clone();
-                stack.push(top_val);
+                let top_val = frame.stack.last().unwrap().clone();
+                frame.stack.push(top_val);
             }
             Command::Plus => {
-                math(&mut stack, MathOp::Plus);
+                math(&mut frame.stack, MathOp::Plus);
             }
             Command::Minus => {
-                math(&mut stack, MathOp::Minus);
+                math(&mut frame.stack, MathOp::Minus);
             }
             Command::Times => {
-                math(&mut stack, MathOp::Times);
+                math(&mut frame.stack, MathOp::Times);
             }
             Command::Divide => {
-                math(&mut stack, MathOp::Divide);
+                math(&mut frame.stack, MathOp::Divide);
             }
             Command::Mod => {
-                math(&mut stack, MathOp::Mod);
+                math(&mut frame.stack, MathOp::Mod);
             }
             Command::Print => {
-                let top_val = stack.last().unwrap();
+                let top_val = frame.stack.last().unwrap();
                 println!("{}", top_val.to_str());
             }
             Command::MkType => {
-                let name = stack.pop().unwrap().must_string();
-                let n_fields = stack.pop().unwrap().must_int();
+                let name = frame.stack.pop().unwrap().must_string();
+                let n_fields = frame.stack.pop().unwrap().must_int();
                 let mut fields = vec![];
                 for _ in 0..n_fields {
-                    fields.push(stack.pop().unwrap().must_string());
+                    fields.push(frame.stack.pop().unwrap().must_string());
                 }
 
                 types.push((name, fields));
-                stack.push(Value::I((types.len() - 1) as i64));
+                frame.stack.push(Value::I((types.len() - 1) as i64));
             }
             Command::New => {
-                let type_id = stack.pop().unwrap().must_int();
+                let type_id = frame.stack.pop().unwrap().must_int();
                 let t = &types[type_id as usize];
                 let val_id = next_obj_id;
                 next_obj_id += 1;
 
                 heap.insert(val_id, t.1.iter().map(default_value).collect());
-                stack.push(Value::R(type_id, val_id));
+                frame.stack.push(Value::R(type_id, val_id));
             }
             Command::GetRef => {
-                let field = stack.pop().unwrap().must_int();
-                let (_, val_id) = stack.pop().unwrap().must_ref();
-                stack.push(heap[&val_id][field as usize].clone());
+                let field = frame.stack.pop().unwrap().must_int();
+                let (_, val_id) = frame.stack.pop().unwrap().must_ref();
+                frame.stack.push(heap[&val_id][field as usize].clone());
             }
             Command::SetRef => {
-                let value = stack.pop().unwrap();
-                let field = stack.pop().unwrap().must_int();
-                let (_, val_id) = stack.pop().unwrap().must_ref();
+                let value = frame.stack.pop().unwrap();
+                let field = frame.stack.pop().unwrap().must_int();
+                let (_, val_id) = frame.stack.pop().unwrap().must_ref();
                 heap.get_mut(&val_id).unwrap()[field as usize] = value;
             }
             Command::SysGC => {
                 let mut live_refs = HashSet::new();
                 let mut to_traverse = VecDeque::new();
 
-                // Collect object roots from the stack
-                for item in stack.iter() {
+                // Collect object roots from the frame.stack
+                for item in frame.stack.iter() {
                     if let Value::R(_, v) = *item {
                         to_traverse.push_back(v);
                     }
@@ -298,7 +352,7 @@ fn interpret(commands: Vec<Command>, names: HashMap<String, usize>) {
 
 fn main() {
     match build(read_lines()) {
-        Ok((commands, names)) => interpret(commands, names),
+        Ok(functions) => interpret(functions),
         Err(err) => println!("Error! {}", err),
     };
 }
